b8e4a06c0cd1a83e898f468f887114a6b58109a0
Kyaw Zay Ya <kyawzaya3578@gmail.com>
2025-11-07 09:53:27 +0700

    edit logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,11 +22,7 @@ function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <div className="flex items-center space-x-2">
-            <img
-              src="/logo.JPG"
-              alt="PetalStars Logo"
-              className="h-10 w-auto"
-            />
+            <h1>🌸</h1>
             <h1 className="text-2xl font-bold text-purple-800">PetalStars</h1>
           </div>
           <div className="hidden md:flex space-x-8">
